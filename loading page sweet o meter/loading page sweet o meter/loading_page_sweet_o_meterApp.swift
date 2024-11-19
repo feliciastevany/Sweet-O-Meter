3819a4ea171a36b211523e759538be3be17065d9
Alfred Hans <112923618+Zwelious@users.noreply.github.com>
2024-11-19 15:02:39 +0700

    Add files via upload

---
+++ loading page sweet o meter/loading page sweet o meter/loading_page_sweet_o_meterApp.swift
@@ -0,0 +1,17 @@
+//
+//  loading_page_sweet_o_meterApp.swift
+//  loading page sweet o meter
+//
+//  Created by student on 19/11/24.
+//
+
+import SwiftUI
+
+@main
+struct loading_page_sweet_o_meterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ loading page sweet o meter/loading page sweet o meterTests/loading_page_sweet_o_meterTests.swift
@@ -0,0 +1,17 @@
+//
+//  loading_page_sweet_o_meterTests.swift
+//  loading page sweet o meterTests
+//
+//  Created by student on 19/11/24.
+//
+
+import Testing
+@testable import loading_page_sweet_o_meter
+
+struct loading_page_sweet_o_meterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
