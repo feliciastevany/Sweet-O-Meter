c13dc307005743e7bc98e5479c007dbc74d064e7
Alfred Hans <112923618+Zwelious@users.noreply.github.com>
2024-11-19 16:31:46 +0700

    Add files via upload

---
+++ Sweet o meter/Sweet o meter/Sweet_o_meterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sweet_o_meterApp.swift
+//  Sweet o meter
+//
+//  Created by student on 19/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Sweet_o_meterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
